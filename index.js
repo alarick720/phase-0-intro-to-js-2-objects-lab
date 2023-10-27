a529fabc2d8f90bec7204b43bd32657319e613b6
Alarick Whitted <awhitted834@gmail.com>
2023-10-27 09:46:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-// Write your solution in this file!
+const employee = {
+    name: "Alarick",
+    streetAddress: "12132 Value Drive"
+}
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+//     employee[key] = value;
+//     return employee;
+// }
+ 
+
+const streetAddress = "11 Broadway"; 
+  updateEmployeeWithKeyAndValue (employee, "streetAdress", streetAddress);
+
+  function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const newemployee = {...employee};
+    newemployee[key] = value;
+    return newemployee
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key]=value;
+    return employee
+
+  }
+  function deleteFromEmployeeByKey(employee, key){
+const newemployee = {...employee};
+delete newemployee[key]
+return newemployee
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+   delete employee[key]
+    return employee;
+
+  }
+function value(value){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  
+     
+ 
+
+
+
+ 
+  
+  
+  
+
+
+
+  
+
+
+
+
+
+
+
